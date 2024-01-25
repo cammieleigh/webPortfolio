73069b717eec0db2b8379f924f7eaee823bbcc98
cammieleighp <cammie.peterson18@gmail.com>
2024-01-24 18:43:33 -0700

    directory changes

--- src/js/page2.js
+++ src/js/page2.js
@@ -4,7 +4,9 @@ import workJson from '../json/work.json';
 
 function Page2() {
 
-    const page2Title = createElement('h1', {className: 'workTitle', textContent:'My Projects!'})
+    const page2Title = createElement('h1', {className: 'workTitle', textContent:'My Projects!'});
+    const subtitle = createElement('h2', {className: 'clarifier', textContent: 'Video Demos or website URLs are included in the read me file:)'});
+    
     const workInfo = createElement('div', {className: 'listCards'});
 
         console.log(workJson);  // temporary checking for valid response and data parsing
@@ -19,13 +21,13 @@ function Page2() {
         name.setAttribute('class', 'workDiv__name');
         name.textContent = item.name;
         card.appendChild(name);
-        let url = document.createElement('a');
-        url.setAttribute('class', 'workDiv__url');
-        url.setAttribute('href', item.url);
-        url.textContent = 'Check out the site!';
-        card.appendChild(url);
-        let enter = document.createElement('br');
-        card.appendChild(enter);
+        // let url = document.createElement('a');
+        // url.setAttribute('class', 'workDiv__url');
+        // url.setAttribute('href', item.url);
+        // url.textContent = 'Check out the site!';
+        // card.appendChild(url);
+        // let enter = document.createElement('br');
+        // card.appendChild(enter);
         let github = document.createElement('a');
         github.setAttribute('class', 'workDiv__github');
         github.setAttribute('href', item.github);
@@ -33,8 +35,8 @@ function Page2() {
         card.appendChild(github);
         workInfo.appendChild(card);
       }
-
-  return createElement('div', {className: "page2div"}, [workInfo, page2Title ]);
+      const page2flex = createElement('div', {className: 'page2flex'}, [workInfo, page2Title]);
+  return createElement('div', {className: "page2div"}, [page2flex, subtitle ]);
 }
 
 export default Page2;
